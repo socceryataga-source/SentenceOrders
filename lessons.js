a99a8a3e3c5c9e203b986cf645f9d7209272ccb6
socceryataga-source <socceryataga@gmail.com>
2026-05-29 16:36:44 +0900

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -1,5 +1,10 @@
 window.LESSONS = {
-  "sample01": [
+  "Sample": [
+    {
+      "en": "I believed that people's destinies depended on when and where they were born.",
+      "jp": "人々の運命は、いつどこで生まれたかによって決まるのだと私は信じていました。",
+      "mode": "word"
+    },
     {
       "en": "I was shocked by the tragic sight.",
       "jp": "私はその悲惨な光景に衝撃を受けました。",
@@ -11,9 +16,39 @@ window.LESSONS = {
       "mode": "word"
     },
     {
-      "en": "I believed that / people's destinies depended on / when and where they were born.",
-      "jp": "人々の運命は、いつどこで生まれたかによって決まるのだと私は信じていました。",
-      "mode": "chunk"
+      "en": "You must become a doctor to help suffering children.",
+      "jp": "あなたは苦しんでいる子どもたちを助けるために医者にならなければなりません。",
+      "mode": "word"
+    },
+    {
+      "en": "My desire to become a doctor and help people in need grew stronger.",
+      "jp": "医者になって困っている人々を助けたいという私の思いはさらに強くなりました。",
+      "mode": "word"
+    },
+    {
+      "en": "I decided to believe in myself and studied hard to improve my grades.",
+      "jp": "私は自分を信じることに決め、成績を上げるために一生懸命勉強しました。",
+      "mode": "word"
+    },
+    {
+      "en": "I obtained my medical license and began working at a hospital.",
+      "jp": "私は医師免許を取得し、病院で働き始めました。",
+      "mode": "word"
+    },
+    {
+      "en": "I decided to work at an emergency center to become a better doctor.",
+      "jp": "よりよい医者になるために、私は救急センターで働くことに決めました。",
+      "mode": "word"
+    },
+    {
+      "en": "My boss challenged me to take action.",
+      "jp": "上司は私に行動を起こすよう促しました。",
+      "mode": "word"
+    },
+    {
+      "en": "Things will work out if you start doing something.",
+      "jp": "何かを始めれば、物事はうまくいくでしょう。",
+      "mode": "word"
     }
   ]
 };
