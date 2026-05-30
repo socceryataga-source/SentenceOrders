67f3853c19e41470ea7dddad707378ed85975f94
socceryataga-source <socceryataga@gmail.com>
2026-05-30 22:35:32 +0900

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -52,3 +52,56 @@ window.LESSONS = {
     }
   ]
 };
+
+"Lesson1-4": [
+  {
+    "en": "In Myanmer / I was / working / completely / on my own / at first",
+    "jp": "ミャンマーで、私は最初完全に自分の力だけで働いた。",
+    "mode": "chunk"
+  },
+  {
+    "en": "But / one person / cannot / do / much / and / I needed / to / be / more organized",
+    "jp": "しかし、一人では多くのことはできないため、より組織的になることが必要だった。",
+    "mode": "chunk"
+  },
+  {
+    "en": "With / my / limited saving / and / some small / donations / I started / a / volunteer / medical organization",
+    "jp": "限られた貯金とわずかの寄付で、私は慈善医療団体をはじめた。",
+    "mode": "chunk"
+  },
+  {
+    "en": "The shocking scene / I / saw / on TV / as a child / led me / to become / a doctor",
+    "jp": "テレビで見た衝撃的な光景が、私を医者に導いた。",
+    "mode": "chunk"
+  },
+  {
+    "en": "When / I look back, / the voice / of my heart / has / always / helped me / to make / important / decisions",
+    "jp": "思い返すと、私の心の声はいつも私が重大な決断をするのを助けてくれた。",
+    "mode": "chunk"
+  },
+  {
+    "en": "Life has many difficulties",
+    "jp": "人生には多くの困難がある。",
+    "mode": "word"
+  },
+  {
+    "en": "When you face them, listen to your heart.",
+    "jp": "それら（困難）に直面した時は、心の声を聞きなさい。",
+    "mode": "word"
+  },
+  {
+    "en": "Things will work out",
+    "jp": "きっとうまくいく。",
+    "mode": "word"
+  },
+  {
+    "en": "If / you start / doing / something, / you will / face / failure / along the way",
+    "jp": "何かをはじめると、その途中で失敗と向き合うことになるだろう。",
+    "mode": "chunk"
+  },
+  {
+    "en": "But remember: / you learn / and grow / through / failure",
+    "jp": "しかし覚えておくべきだ。人々は失敗から学び、成長するということを。",
+    "mode": "chunk"
+  }
+]
